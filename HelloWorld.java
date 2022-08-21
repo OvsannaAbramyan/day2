4583825f63cc0b9179ecb3adadd9686f7fa76bd2
ovsanna <ovsanna@lionsinserance.com>
2022-08-21 14:12:23 -0700

    HelloWorld.java is created.

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld {r
+    public static void main(String[] args) {
+
+        System.out.println("Good old Days");
+
+// some work here.
+
+    }
+}
+
+    public static void main(String[] args) {
+        System.out.println("idk");
+
+        // hello world is updated
+
+    }
+}
